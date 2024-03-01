aa529806f21b8c1ca24e9734b0bae8bf306c9dc4
Zach Gharst <zdgharst@gmail.com>
2024-03-01 10:24:20 -0600

    Remove different background for nno-current windows

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -9,6 +9,7 @@ function ApplyVimTheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 end
 
 ApplyVimTheme()
