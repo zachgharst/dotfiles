756537958eb916434a49fc0d4be2baf36f7ef240
Zach Gharst <zdgharst@gmail.com>
2024-03-22 07:31:10 -0500

    Use defaults again

--- nvim/after/plugin/neotest.lua
+++ nvim/after/plugin/neotest.lua
@@ -4,16 +4,6 @@ neotest.setup({
   adapters = {
     require('neotest-dotnet')
   },
-  -- i'm not a huge fan of these icons, but i don't like the defaults either
-  -- maybe try just letters some day
-  icons = {
-    failed = "❌",
-    passed = "✅",
-    running = "😥",
-    skipped = "⏭️",
-    unknown = "❔",
-    watching = "👀",
-  },
 })
 
 vim.keymap.set('n', '<leader>tt', function() neotest.run.run() end)
