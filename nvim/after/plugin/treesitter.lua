f969a7ff55e85ce124e83fc066c02efed81aee1e
Zach Gharst <zdgharst@gmail.com>
2024-12-08 19:12:41 -0600

    Clean up treesitter config

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,24 +1,32 @@
-require 'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (c, lua, vim, vimdoc, and query should always be installed)
-  ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "json", },
+local required_parsers = {
+  -- these should always be intalled; required by nvim/treesitter
+  "c",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "markdown",
+  "markdown",
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
+  -- optional
+  "javascript",
+  "typescript",
+  "json",
+}
+
+require('nvim-treesitter.configs').setup({
+  ensure_installed = required_parsers,
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
   highlight = {
     enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
+    disable = function(lang, buf)
+      local max_filesize = 1024 * 1024 -- 1 MB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
     additional_vim_regex_highlighting = false,
   },
-}
+})
