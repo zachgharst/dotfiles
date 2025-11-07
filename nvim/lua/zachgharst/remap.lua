63b69e0b7f6315e36837d408bf93b1415a6f65cd
Zach Gharst <zdgharst@gmail.com>
2025-11-07 00:23:48 -0600

    Remove mappings I never use

--- nvim/lua/zachgharst/remap.lua
+++ nvim/lua/zachgharst/remap.lua
@@ -26,8 +26,6 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>dot", "<cmd>e $DOTFILES<CR>");
-vim.keymap.set("n", "<leader>vpp", "<cmd>e $DOTFILES/nvim/lua/zachgharst/packer.lua<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
