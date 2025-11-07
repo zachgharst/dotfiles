083bc296e7d6f8bf9facaf9df61a2c7c037aa1ea
Zach Gharst <zdgharst@gmail.com>
2025-11-07 00:56:33 -0600

    Move obsidian to lazy

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,42 @@
+return {
+  "epwalsh/obsidian.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = os.getenv("VAULT"),
+      },
+    },
+    daily_notes = {
+      folder = "dailies",
+      alias_format = "%A, %B %-d, %Y",
+      template = "daily.md",
+    },
+    templates = {
+      subdir = "templates",
+      substitutions = {
+        yesterday = function()
+          return os.date("%Y-%m-%d", os.time() - 86400)
+        end,
+        tomorrow = function()
+          return os.date("%Y-%m-%d", os.time() + 86400)
+        end,
+        mmddyyyy = function()
+          return os.date("%m-%d-%Y")
+        end,
+      },
+    },
+  },
+  keys = {
+    { "<leader>oo", vim.cmd.ObsidianQuickSwitch, desc = "Obsidian: Quick Switch" },
+    { "<leader>on", vim.cmd.ObsidianNew, desc = "Obsidian: New Note" },
+    { "<leader>ot", vim.cmd.ObsidianToday, desc = "Obsidian: Today's Note" },
+    { "<leader>oy", vim.cmd.ObsidianYesterday, desc = "Obsidian: Yesterday's Note" },
+    { "<leader>or", vim.cmd.ObsidianTomorrow, desc = "Obsidian: Tomorrow's Note" },
+    { "<leader>od", vim.cmd.ObsidianDailies, desc = "Obsidian: Dailies" },
+    { "<leader>os", vim.cmd.ObsidianSearch, desc = "Obsidian: Search" },
+    { "<leader>oe", vim.cmd.ObsidianExtractNote, mode = "v", desc = "Obsidian: Extract Note" },
+    { "<leader>oi", vim.cmd.ObsidianPasteImg, mode = "v", desc = "Obsidian: Paste Image" },
+  },
+}
