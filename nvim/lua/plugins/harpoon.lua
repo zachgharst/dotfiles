ad4a56721ec630216f3cb23aa9f01b191c98bfa7
Zach Gharst <zdgharst@gmail.com>
2026-01-05 23:25:00 -0600

    Change harpoon's config to match a lazy style

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,21 +1,3 @@
-local harpoon = require("harpoon")
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
-end
-
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
@@ -23,13 +5,32 @@ return {
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope.nvim" },
   },
-  keys = {
-    { "<leader>a", function() harpoon:list():add() end, desc = "Harpoon: Add File" },
-    -- { "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Harpoon: Toggle Quick Menu" },
-    { "<C-e>", function() toggle_telescope(harpoon:list()) end, desc = "Harpoon: Toggle Quick Menu" },
-    { "<C-h>", function() harpoon:list():select(1) end, desc = "Harpoon: Navigate to File 1" },
-    { "<C-j>", function() harpoon:list():select(2) end, desc = "Harpoon: Navigate to File 2" },
-    { "<C-k>", function() harpoon:list():select(3) end, desc = "Harpoon: Navigate to File 3" },
-    { "<C-l>", function() harpoon:list():select(4) end, desc = "Harpoon: Navigate to File 4" },
-  },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
+    end
+
+    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Harpoon: Toggle Quick Menu" })
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon: Add File" })
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Harpoon: Navigate to File 1" })
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = "Harpoon: Navigate to File 2" })
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, { desc = "Harpoon: Navigate to File 3" })
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, { desc = "Harpoon: Navigate to File 4" })
+    vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle Quick Menu" })
+  end,
 }
