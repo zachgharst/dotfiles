90430680af5ffaf50c9e545332cee7e473dc0060
Zach Gharst <zdgharst@gmail.com>
2026-01-05 23:24:52 -0600

    Use latest telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
-  tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<C-p>", "<cmd>Telescope git_files<CR>", desc = "Telescope: Git Files" },
