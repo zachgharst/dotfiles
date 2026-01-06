8592cabca0ac2c28b342c8092fa761118d6fbcb1
Zach Gharst <zdgharst@gmail.com>
2026-01-05 23:38:59 -0600

    Migrate theme to lazy

---
+++ nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,20 @@
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+      local rose_pine = require("rose-pine")
+      rose_pine.setup({
+        styles = {
+          -- todo: set this to true and change what I want to be italic
+          italic = false,
+          transparency = true,
+        },
+      })
+
+	  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
+      vim.cmd("colorscheme rose-pine")
+	end,
+}
