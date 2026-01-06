1f2cda8a4d05ef7d161e333ae24bd4b7f7c464c4
Zach Gharst <zdgharst@gmail.com>
2026-01-06 00:15:37 -0600

    Fix formatting

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -2,19 +2,19 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
-      local rose_pine = require("rose-pine")
-      rose_pine.setup({
-        styles = {
-          -- todo: set this to true and change what I want to be italic
-          italic = false,
-          transparency = true,
-        },
-      })
+    local rose_pine = require("rose-pine")
+    rose_pine.setup({
+      styles = {
+        -- todo: set this to true and change what I want to be italic
+        italic = false,
+        transparency = true,
+      },
+    })
 
 	  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 
-      vim.cmd("colorscheme rose-pine")
+    vim.cmd("colorscheme rose-pine")
 	end,
 }
