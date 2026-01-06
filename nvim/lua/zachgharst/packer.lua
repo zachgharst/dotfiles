2c41ce6d09580dc30a093e5b6e2e7d61be08a208
Zach Gharst <zdgharst@gmail.com>
2026-01-06 00:17:49 -0600

    Remove packer stuff

--- nvim/lua/zachgharst/packer.lua
+++ nvim/lua/zachgharst/packer.lua
@@ -4,25 +4,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use('wbthomason/packer.nvim')
 
-  use({
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  })
-
-  use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-    end
-  })
-
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
   use('Olical/conjure')
   use('othree/html5.vim')
   use('pangloss/vim-javascript')
@@ -31,7 +12,6 @@ return require('packer').startup(function(use)
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn['mkdp#util#install']() end,
   })
-  use('github/copilot.vim')
 
   use {
     'VonHeikemen/lsp-zero.nvim',
@@ -54,16 +34,6 @@ return require('packer').startup(function(use)
     }
   }
 
-  use 'stevearc/dressing.nvim'
-  use({
-    'ziontee113/icon-picker.nvim',
-    config = function()
-      require('icon-picker').setup({
-        disable_legacy_commands = true
-      })
-    end,
-  })
-
   use({
     'folke/trouble.nvim',
     requires = { { 'nvim-tree/nvim-web-devicons' } }
@@ -78,15 +48,6 @@ return require('packer').startup(function(use)
       'nvim-treesitter/nvim-treesitter',
       'Issafalcon/neotest-dotnet',
     }
-  })
-
-  use({
-    "epwalsh/obsidian.nvim",
-    tag = "*", -- recommended, use latest release instead of latest commit
-    requires = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-    },
   })
 
   use('mrcjkb/rustaceanvim')
