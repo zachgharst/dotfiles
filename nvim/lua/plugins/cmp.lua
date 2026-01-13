57a8398d79b7c2c5f90ecdc801920a5e580a69f8
Zach Gharst <zdgharst@gmail.com>
2026-01-13 00:47:20 -0600

    Update completion sources

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -25,6 +25,7 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-CR>"] = function(fallback)
           cmp.abort()
@@ -38,6 +39,11 @@ return {
         } or false,
       },
       sorting = defaults.sorting,
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+      },
     }
   end,
 }
