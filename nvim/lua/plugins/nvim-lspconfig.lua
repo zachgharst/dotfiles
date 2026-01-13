86edeafee8d0679d4082aa57b737a3c0acc1d179
Zach Gharst <zdgharst@gmail.com>
2026-01-13 00:47:30 -0600

    Fix lsp config (whew)

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,20 +1,33 @@
-return   {
+return {
   "neovim/nvim-lspconfig",
   lazy = false,
-  "mason.nvim",
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "omnisharp",
-        "lua_ls",
+  dependencies = {
+    "mason.nvim",
+    {
+      "mason-org/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = {
+          "omnisharp",
+          "lua_ls",
+        },
       },
-    },
+    }
   },
   config = function()
     vim.lsp.config['omnisharp'] = {
       filetypes = { "cs" },
       cmd = { "OmniSharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
     }
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.lsp_definitions() end)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
   end,
 }
